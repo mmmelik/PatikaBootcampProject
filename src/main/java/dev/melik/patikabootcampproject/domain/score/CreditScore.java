0b45a390f16574880896f4f2046fb047be9e467f
Melik Mehmet Özyildirim <melikozy@gmail.com>
2022-02-12 16:51:44 +0300

    Constructors needed for caching.

--- src/main/java/dev/melik/patikabootcampproject/domain/score/CreditScore.java
+++ src/main/java/dev/melik/patikabootcampproject/domain/score/CreditScore.java
@@ -1,14 +1,14 @@
 package dev.melik.patikabootcampproject.domain.score;
 
-import lombok.Builder;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 import java.io.Serializable;
 
 @Getter
 @Setter
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class CreditScore implements Serializable {
 
     private Long id;
