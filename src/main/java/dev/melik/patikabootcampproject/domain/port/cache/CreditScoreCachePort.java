85423f827aa92ef3ca48efe91d480eb252e343d8
Melik Mehmet Özyildirim <melikozy@gmail.com>
2022-02-13 13:17:55 +0300

    cleanup

--- src/main/java/dev/melik/patikabootcampproject/domain/port/cache/CreditScoreCachePort.java
+++ src/main/java/dev/melik/patikabootcampproject/domain/port/cache/CreditScoreCachePort.java
@@ -2,7 +2,6 @@ package dev.melik.patikabootcampproject.domain.port.cache;
 
 import dev.melik.patikabootcampproject.domain.score.CreditScore;
 
-import javax.swing.text.html.Option;
 import java.util.Optional;
 
 public interface CreditScoreCachePort {
